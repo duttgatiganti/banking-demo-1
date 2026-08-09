c3f9ee2756c499a4ea7bda3867a431cba43e1fde
duttgatiganti <116521479+duttgatiganti@users.noreply.github.com>
2026-08-09 17:23:47 +0530

    Update docker.groovy

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,24 +1,21 @@
-def call(String imageName, String credentialId) {
+def call(String imageName, String credentialId, String username) {
 
     withCredentials([
-        usernamePassword(
+        string(
             credentialsId: credentialId,
-            usernameVariable: 'DOCKER_USERNAME',
-            passwordVariable: 'DOCKER_PASSWORD'
+            variable: 'DOCKER_PASSWORD'
         )
     ]) {
 
         sh """
             set -e
 
-            echo "Building image: ${imageName}:${env.BUILD_NUMBER}"
-
             docker build -t ${imageName}:${env.BUILD_NUMBER} .
 
             docker images
 
             echo "\$DOCKER_PASSWORD" | docker login \
-                -u "\$DOCKER_USERNAME" \
+                -u "${username}" \
                 --password-stdin
 
             docker push ${imageName}:${env.BUILD_NUMBER}
